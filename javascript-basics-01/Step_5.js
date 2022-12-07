c51985a9571f7cce3bf30c510c7bcc9f7e0312e3
Aghazzi <aghazzi.1998@gmail.com>
2022-12-07 19:10:48 +0200

    completed Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,4 @@
+document.getElementById("validate").onclick = function() {
+    first_number=document.getElementById("first_number").value, second_number=document.getElementById("second_number").value;
+    alert(first_number +"X"+ second_number+"="+ first_number*second_number);
+}
