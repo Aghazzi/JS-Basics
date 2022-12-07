d4ad0881f24b3ce72bf6ba54eb400f92aaa805dd
Aghazzi <aghazzi.1998@gmail.com>
2022-12-07 22:40:07 +0200

    completed Step_8 and Basics1

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,9 @@
+document.getElementById("validate").onclick = function() {
+    age=document.getElementById("age").value;
+    if (age >= 18) {
+        alert("You are over 18")
+    }
+    else {
+        alert("You are under 18")
+    }
+}
