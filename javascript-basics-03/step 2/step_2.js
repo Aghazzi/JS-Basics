af162cd24de8747398faf11199a583ac21052404
Aghazzi <aghazzi.1998@gmail.com>
2022-12-09 09:55:17 +0200

    fixed step_2

--- javascript-basics-03/step 2/step_2.js
+++ javascript-basics-03/step 2/step_2.js
@@ -1,19 +1,42 @@
-var showLink = document.getElementById("show");
-var hideLink = document.getElementById("hide");
-var parag = document.getElementById('texte')
+// var showLink = document.getElementById("show");
+// var hideLink = document.getElementById("hide");
+// var parag = document.getElementById('texte')
 
-showLink.onclick = () => {
-    showHideText('show')
-}
+// showLink.onclick = () => {
+//     showHideText('show')
+// }
 
-hideLink.onclick = () => {
-    showHideText('hide')
-}
+// hideLink.onclick = () => {
+//     showHideText('hide')
+// }
 
-const showHideText = (name) => {
-    if(name == 'show') {
-        parag.style.display = 'block'
-    } else {
-        parag.style.display = 'none'
+// const showHideText = (name) => {
+//     if(name == 'show') {
+//         parag.style.display = 'block'
+//     } else {
+//         parag.style.display = 'none'
+//     }
+// }
+document.querySelectorAll("a").forEach(e => {
+    e.onclick=(e)=>{
+        if(e.target.id=="show"){
+            document.getElementById("texte").style="color:black;"
+        }
+        else if(e.target.id=="hide"){
+            document.getElementById("texte").style.color="transparent"
+        }
     }
-}
+});
+// const show_hide = (show , hide , targetText) => {
+//     show.onclick = () => {
+//         targetText.style.display = "block"
+//     }
+//     hide.onclick = () => {
+//         targetText.style.display = "none"
+//     }
+// }
+
+// const show = document.getElementById('show')
+// const hide = document.getElementById('hide')
+// const text = document.getElementById('texte')
+// show_hide(show , hide , text)
